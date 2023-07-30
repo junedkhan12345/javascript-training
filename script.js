7b61ce3c31ee3e80e81100d84f9bf4ebbec07965
junedkhan12345 <135973290+junedkhan12345@users.noreply.github.com>
2023-07-30 21:52:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// let arr = [3, 5, 4, 17, 47, 45, 30];
+// console.log(arr);
+// push function array me last me add karega
+// arr.push(2);
+// arr.push(100);
+// console.log(arr);
+// pop function array me last se delete karega
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+// unshift function array me first me add karega
+// arr.unshift(10);
+// arr.unshift(20);
+// console.log(arr);
+// shift function array me first se delete karega
+// arr.shift();
+// arr.shift();
+// console.log(arr);
+// toString function array ko string me converd karega
+// console.log(arr.toString());
+// join function
+// console.log(arr.join("*"));
+// console.log(arr.join("###"));
+// reserve function array ko ulta ar dega
+// console.log(arr.reverse());
+// at function
+// console.log(arr.at(0));
+// console.log(arr.at(5));
+// console.log(arr.at(arr.length - 1));
+// concat function
+// let arr2 = [3, 4, 3, 34, 45, 54, 6, 76, 43];
+// console.log(arr2);
+// let result = arr.concat(arr2);
+// console.log(result);
+
+// let arr = [3, 5, 4, 26, 47, 45, 30];
+// console.log(arr);
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 1) {
+//     ++count;
+//   }
+// }
+// console.log(count);
