16ba61d91f7c5caaaf1d86b887de038076432a6e
junedkhan12345 <135973290+junedkhan12345@users.noreply.github.com>
2023-08-30 10:55:28 +0530

    Add files via upload

---
+++ exercise while loop/js/script.js
@@ -0,0 +1,153 @@
+// Write a while loop that logs the numbers 1 to 10 in the console.
+
+// let i = 1;
+// while(i <= 10){
+//     console.log(i)
+//     i++;
+// }
+
+// Calculate sum of all numbers from 1 to 10
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 10){
+//     sum = sum + i;
+   
+//     i++;
+// }
+// console.log(sum);
+
+
+// Counting down from 10 to 1
+// let i = 10;
+// while(i >= 1){
+//     console.log(i);
+//     i--;
+// }
+
+// print 2's table
+// let x = 2;
+// let i = 1;
+// while (i <= 10) {
+//   console.log(i * x);
+//   i++;
+// }
+
+// Calculate sum of all odd numbers from 1 to 100
+
+// let i = 1;
+// let sum = 0;
+// while(i <= 100){
+//     if(i % 2 ===1){
+//         sum = sum + i;
+// }
+    
+//     i++;
+// }
+//     console.log(sum);
+
+// Calculate sum of all odd numbers from 1 to 100
+
+//  i = 1;
+//  sum = 0;
+//  while(i <= 100){
+//     if(i % 2 === 0){
+//         sum = sum + i;
+//     }
+//     i++;
+//  }
+//  console.log(sum);
+
+
+
+// Write a do...while loop that logs the numbers 1 to 5 in the console
+// let i = 1;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i <= 5)
+
+
+// Print all Odd numbers from 1 to 100
+
+// let i = 1;
+// do{
+//     console.log(i);
+//     i = i + 2;
+// }while(i <= 100)
+
+// Print all even numbers from 1 to 100
+
+// let i = 2;
+// do{
+//     console.log(i);
+//     i = i + 2;
+// }while(i <= 100)
+
+
+// print multiple of 5 from 1 to 100
+
+// let i = 5;
+// do{
+//     console.log(i);
+//     i = i + 5;
+    
+// }while(i <= 100)
+
+
+// Print all numbers which are divisible of 5 and 15 in 1 to 100
+
+// let i = 1;
+// do{
+//     if(i % 5 === 0 && i % 15 === 0){
+//         console.log(i);
+//     }
+//     i++;
+// }while(i <= 100)
+
+
+// Print 2's table
+
+// let x = 2;
+// let i = 1;
+// do{
+//     console.log(i * x);
+//     i++;
+// }while(i <= 10)
+
+
+// Ask user a question "How many times do you want to print Hello World?" and then print Hello World that number of times
+
+// let count = prompt("How many times do you want to print Hello World?");
+// let i = 1;
+// do{
+//     console.log("hello world :" +i);
+//     i++;
+// }while(i <= count)
+
+
+// Calculate sum of all even numbers from 1 to 100
+
+// let sum = 0;
+// let i = 1;
+// do{
+//     if(i % 2 === 0){
+//         sum = sum + i;
+//     }
+//     i++;
+// }while(i <= 100)
+// console.log(sum);
+
+
+// Calculate sum of all odd numbers from 1 to 100
+
+// let sum = 0;
+// let i = 1;
+// do{
+//     if(i % 2 === 1){
+//         sum = sum + i;
+//     }
+//     i++;
+// }while(i <= 100)
+// console.log(sum);
+
