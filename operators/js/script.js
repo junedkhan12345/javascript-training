7d0629257617d116e323573a5f19108de9002f83
junedkhan12345 <135973290+junedkhan12345@users.noreply.github.com>
2023-08-30 10:44:22 +0530

    Add files via upload

---
+++ operators/js/script.js
@@ -0,0 +1,24 @@
+console.log(2 + 3 * 6);
+console.log(4 * 4 + 7);
+console.log((4 * 2) / 5);
+console.log(7 % 64);
+console.log((4 > 5 && 5 === 5 && 10 > 5)|| 10 >= 10);
+console.log(!(4 > 5 && 5 === 5 && 10 > 5)&& 10 < 10);
+console.log(!(4 < 3));
+console.log(!(4 > 3));
+
+
+//typeof operator(check whether type of data)
+console.log(typeof 10);   //number datatype
+console.log(typeof "wecode");   //string datatype
+console.log(typeof true);   //boolean datatype
+console.log(typeof age);   //undefined datatype
+
+console.log(typeof "10");   //string datatype
+console.log(typeof wecode);   //undefined datatype
+console.log(typeof (4 < 5));   //boolean datatype
+
+
+console.log(Number("wecode"));   //naN(not a number)
+
+
