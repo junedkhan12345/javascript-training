e64064f9ba3af944de8a1817c307f21feb24a9b9
junedkhan12345 <135973290+junedkhan12345@users.noreply.github.com>
2023-08-30 10:34:09 +0530

    Add files via upload

---
+++ array/js/script.js
@@ -0,0 +1,39 @@
+// //print odd even numbers
+let Numbers = [1, 2, 3, 4, 5];
+// for (let i = 0; i < Numbers.length; i++) {
+//   if (Numbers[i] % 2 === 1) {
+//     console.log("ODD");
+//   } else {
+//     console.log("EVEN");
+//   }
+// }
+
+// //print sum of numbers
+// let sum = 0;
+// for (let i = 0; i < Numbers.length; i++) {
+//   sum = sum + Numbers[i];
+// }
+// console.log(sum);
+
+// //print sum of odd numbers
+// let oddSum = 0;
+// for (let i = 0; i < Numbers.length; i++) {
+//   if (Numbers[i] % 2 === 1) {
+//     oddSum = oddSum + Numbers[i];
+//   }
+// }
+// console.log(oddSum);
+
+// //print sum of even numbers
+// let evenSum = 0;
+// for (let i = 0; i < Numbers.length; i++) {
+//   if (Numbers[i] % 2 === 0) {
+//     evenSum = evenSum + Numbers[i];
+//   }
+// }
+// console.log(evenSum);
+
+// // print first number
+console.log(Numbers[0]);
+//print last number
+console.log(Numbers[4]);
