5e40b73213456736e58f48f2e3d599c934da2b8e
junedkhan12345 <135973290+junedkhan12345@users.noreply.github.com>
2023-08-30 10:37:04 +0530

    Add files via upload

---
+++ loops/js/script.js
@@ -0,0 +1,60 @@
+// for(let i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+
+
+// for(let i = 2; i <= 100; i = i + 2){
+//     console.log(i);
+// }
+
+// for(let i = 5; i <= 50; i = i + 5){
+//     console.log(i);
+// }
+
+// for(let i = 50; i >= 5; i = i - 5){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 99; i = i + 2){
+//     console.log(i);
+// }
+
+// for(let i = 0; i <= 100; i = i + 2){
+//     console.log(i);
+// }
+
+
+// for(let i = 7; i <= 100; i = i + 7){
+//     console.log(i);
+// }
+
+// for(let i = 1; i <= 5; i++){
+//     for(let i = 1; i <=5; i++){
+//        console.log("*");
+//     }
+// }
+
+
+// for(let i = 1; i <= 5; i++){
+//     for(let i = 1; i <=5; i++){
+//         for(let i = 1; i <=5; i++){
+//             console.log("*");
+//         }
+//     }
+// }
+
+
+// let i = 1;
+// while(i <= 15){
+//     console.log(i);
+//     i++
+// }
+
+
+let i = 1;
+while(1 <= 100){
+    if(i % 7 ===0){
+        console.log(i);
+    }
+    i++
+}
